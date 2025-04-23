cead84937dedb7ac71859188ff9f922938360656
kavancamp <keenahvancampenhout@gmail.com>
2025-04-23 13:42:16 -0500

    added particles to main

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shots import Shots
+from particle import Particle
+
 
 def main():
     pygame.init()
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,17 +17,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     # create groups
     updatable = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+    particles = pygame.sprite.Group()
 
     Player.containers = (updatable, drawables) #tells player to add itself to groups when initialized
     Asteroid.containers = (asteroids, updatable, drawables) #every instance of asteroid added automatically
     AsteroidField.containers = updatable
     Shots.containers = (updatable, drawables, shots)
     asteroid_field = AsteroidField()
+    Asteroid.particle_group = particles
     # Instantiate game objects
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2) #instantiate player
     
@@ -41,6 +47,7 @@ def main():
         # Update player input/logic
         #player.update(dt)
         updatable.update(dt) #update all updateable objects
+        particles.update(dt)
         # collision check
         for asteroid in asteroids:
             if player.collides_with(asteroid):
@@ -48,16 +55,20 @@ def main():
                 pygame.quit()
                 exit()
         for asteroid in asteroids:
-            for bullet in shots:
-                if bullet.collides_with(asteroid):
-                    bullet.kill()
-                    asteroid.kill()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    shot.kill()
+                    asteroid.split()
+                break
+        
 
         # Clear screen/black
         screen.fill((0, 0, 0))
         # draw drawable objects
         for drawable in drawables:
             drawable.draw(screen)
+        for p in particles:
+            p.draw(screen)
         # update display
         pygame.display.flip()
 
