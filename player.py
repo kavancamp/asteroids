b0ec824257c3c0c47c72a06f57ce011a6481227b
kavancamp <keenahvancampenhout@gmail.com>
2025-04-22 19:31:11 -0500

    fixed shoot method/added velocity

--- player.py
+++ player.py
@@ -1,13 +1,11 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
-
+from constants import *
+from shots import Shots
 
 class Player(CircleShape):
     def __init__(self, x, y):  # <-- Accept x and y as arguments
         super().__init__(x, y, PLAYER_RADIUS)
-        self.x = x
-        self.y = y
         self.rotation = 0
 
     
@@ -22,9 +20,6 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width=2)
-    
-    def rotate(self, dt):
-       self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -37,11 +32,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
-    
+    def rotate(self, dt):
+       self.rotation += PLAYER_TURN_SPEED * dt
+
     def move(self, dt):
         #  unit vector pointing up/rotate based on player's rotation
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         #  scale the vector by speed and delta time/move the player
         self.position += forward * PLAYER_SPEED * dt
-       
+    
+    def shoot(self):
+        shot = Shots(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
